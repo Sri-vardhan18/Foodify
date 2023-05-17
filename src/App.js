5912e2eb7a36bf2125808cb541e301df0f864610
srivardhan <srivardhanpusala18@gmail.com>
2023-05-17 11:58:49 +0530

    foodify project

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Fragment, useState } from 'react';
+
+import Header from './Components/Layout/Header';
+import Meals from './Components/Meal/Meals';
+import Cart from './Components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals/>
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
+
+
